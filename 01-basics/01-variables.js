49b5e1920e657954e3836dcb6d430d89210848a5
Muhammad Rafay Dogar <rafaydogar93@gmail.com>
2024-07-28 16:52:56 +0500

    vairiables

---
+++ 01-basics/01-variables.js
@@ -0,0 +1,13 @@
+const accountId = 144553
+let accountEmail= "rafay@google.com"
+var accountPassword = "12345"
+accountCity = "sahiwal"
+
+accountEmail = "rd@google.com"
+accountPassword = "252525"
+accountCity = "lahore"
+let accountState;
+//accountId = 2
+console.log(accountId);
+console.table([accountId, accountEmail, accountPassword, accountCity, accountState])
+
